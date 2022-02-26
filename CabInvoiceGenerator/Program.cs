c76dc78488f5ffb3b99827152bdc39f90ac0390d
aamir0143 <mdaamirreza00@gmail.com>
2022-02-26 13:02:48 +0530

    [Aamir] Add : Displaying Welcome Message For Cab Invoice Generator

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Displaying The Welcome Message.
+            Console.WriteLine("==========Welcome To Cab Invoice Generator Programs==========");
+            Console.ReadLine();
+        }
+    }
+}
